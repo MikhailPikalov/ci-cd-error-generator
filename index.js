c8130e56a13cecbe22b01404897b80741091d894
Mikhail Pikalov <mikhail.pikalov@gmail.com>
2021-11-11 00:17:19 +0100

    add random error generation

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+  
+  const seed = Math.random();
+  
+  if (seed > 0.75) {
+    throw new Exception(`A thrown exception from the app, seed was ${seed}`);
+  }
+  
+  if (seed > 0.5) {
+    console.error(`A console error log, seed was ${seed}`);
+  }
 });
 
 app.listen(port, () => {
